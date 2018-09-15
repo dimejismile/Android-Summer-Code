f3408168c42505ce04e6a51cef43868041832ccc
Oladimeji Smile <dimejiridwan@hotmail.com>
2018-09-15 02:30:22 +0100

    Concatenation Exercise
    Removing the 'name' attribute
    Replacing with 'firstName' and 'lastName'
    Create getter and setter for firstName and lastName
    Create getFullName() to concatenate both firstName and lastName
    This exercise can be found in unit 7

--- getterandsetter/src/com/makotojava/intro/Person.java
+++ getterandsetter/src/com/makotojava/intro/Person.java
@@ -5,28 +5,50 @@ import java.util.logging.Logger;
 public class Person {
 	Logger l = Logger.getLogger(Person.class.getName());
 	
-	private String name;
+	//private String name
+	private String firstName;
+	private String lastName;
 	private int age;
 	private int height;
 	private int weight;
 	private String eyecolor;
 	private String gender;
 	
-public Person (String name, int age, int height, int weight, String eyecolor, String gender) {
-	this.name= name;
+public Person (String firstName, String lastName, int age, int height, int weight, String eyecolor, String gender) {
+	this.firstName= firstName;
+	this.lastName= lastName;
 	this.age = age;
 	this.height = height;
 	this.weight = weight;
 	this.eyecolor = eyecolor;
 	this.gender = gender;
 }
-	
-	public String getName() {
+	//Concatenation Exercise
+	public String getFirstName() {
+	return firstName;
+}
+
+	public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+	public String getLastName() {
+	return lastName;
+}
+
+	public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+	public String getFullName() {
+		return getFirstName().concat(" ").concat(getLastName());
+	}
+
+	/*public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
+	}*/
 	public int getAge() {
 		return age;
 	}
