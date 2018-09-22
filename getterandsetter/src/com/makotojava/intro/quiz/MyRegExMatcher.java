6d0d12eecb5361cd58b8d8e7f34e4d2b8a843f3d
Oladimeji Smile <dimejiridwan@hotmail.com>
2018-09-22 14:25:27 +0100

    Unit 19 Question 9

--- getterandsetter/src/com/makotojava/intro/quiz/MyRegExMatcher.java
+++ getterandsetter/src/com/makotojava/intro/quiz/MyRegExMatcher.java
@@ -1,12 +1,19 @@
 package com.makotojava.intro.quiz;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-//Unit 19 Question 7
+//Unit 19 Question 9
 //Declaring the class 'MyRegExMatcher'
 public class MyRegExMatcher {
 
 	//Declaring the method 'matchesAll' and the two parameters 'regex' and 'input'
 	public boolean matchesAll(String regEx, String input) {
-		boolean ret = false;
-		return ret;
+		boolean ret = true;
+		
+		Pattern pattern = Pattern.compile(regEx);
+	      Matcher matcher = pattern.matcher(input);
+	       
+	      ret = matcher.matches();
+	      return ret;
 	}
 }
